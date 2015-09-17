6a14765430786f6c7fcddd3b1361df25225eed81
anderson7gomes <gomes.andrade1@gmail.com>
2015-09-16 21:49:39 -0300

    Exemplo simples de teste com JUnit

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,59 +1,28 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
 
+	@Test
 	public void testAdd() {
-	
+		
 		Calculator calc = new Calculator();
 		
-		double result = calc.add(10.0, 50.0);
+		double result = calc.add(50.0, 10.0);
 		
-		if (result != 60.0) {
-			throw new IllegalStateException("Bad result: " + result);
-		}
+		assertEquals(60.0, result, 0.0);
 		
-		System.out.print('.');
-	
 	} // end testAdd method
 	
+	@Test
 	public void testMultiply() {
-	
+		
 		Calculator calc = new Calculator();
 		
 		double result = calc.multiply(3.0, 4.0);
 		
-		if (result != 12.0) {
-			throw new IllegalStateException("Bad result: " + result);
-		}
+		assertEquals(12.0, result, 0.0);
 		
-		System.out.print('.');
-	
 	} // end testMultiply method
-
-	public static void main(String[] args) {
 	
-		CalculatorTest test = new CalculatorTest();
-		
-		int numberOfErrors = 0;	
-			
-		try {
-			test.testAdd();
-		} catch (Throwable e) {
-		
-			++numberOfErrors;
-			System.out.print('E');
-
-		}
-		
-		try {
-			test.testMultiply();
-		} catch (Throwable e) {
-		
-			++numberOfErrors;
-			System.out.print('E');
-
-		}
-		
-		System.out.printf("\n\nOk <%d errors>\n", numberOfErrors);
-	
-	} // end main method
-
 } // end CalculatorTest class
