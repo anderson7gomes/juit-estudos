025ab679f63b7876888de2d53d3c69c8ee61f30b
anderson7gomes <gomes.andrade1@gmail.com>
2015-09-17 23:44:30 -0300

    Exemplo de test suite com o test runner Suite

---
+++ EmployeeTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class EmployeeTest {
+
+	@Test
+	public void testGetName() {
+		
+		Employee employee = new Employee("Anderson");
+	
+		assertEquals("Name not equals", "Anderson", 
+				employee.getName());
+		
+	} // end testGetName method
+	
+} // end EmployeeTest class
