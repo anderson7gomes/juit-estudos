585df3dfbcd7576fd671511481eb5cf38aed1908
anderson7gomes <gomes.andrade1@gmail.com>
2015-10-22 21:16:53 -0300

    Redemonstrando o runner Suite

---
+++ src/test/java/example/others/SuiteDemo.java
@@ -0,0 +1,11 @@
+package example.others;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestA.class, TestB.class})
+public class SuiteDemo {
+
+} // end SuiteDemo class
---
+++ src/test/java/example/others/TestB.java
@@ -0,0 +1,15 @@
+package example.others;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TestB {
+
+	@Test
+	public void test() {
+		
+		assertTrue(true);
+		
+	} // end test method
+	
+} // end TestB class
